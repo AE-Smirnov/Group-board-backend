8936af4fed042377c6250be0eb7e539956167234
mariia <mariia.shadricheva@ori.team>
2020-07-10 13:24:27 +0300

    removing port

--- main.py
+++ main.py
@@ -54,5 +54,4 @@ def build_preflight_response():
     return response
 
 # Bind to PORT if defined, otherwise default to 5000.
-port = int(os.environ.get('PORT', 5005))
-app.run(host='0.0.0.0', port=port)
+app.run()
