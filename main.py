e1af92f5388f6bca0b225f54e165681d2827e5d4
mariia <mariia.shadricheva@ori.team>
2020-07-10 12:58:22 +0300

    trying to run

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, make_response, request
 import api
 import json
 import flask_cors
+import os
 
 
 class App(Flask):
@@ -50,4 +51,7 @@ def build_preflight_response():
     response.headers.add('Access-Control-Allow-Methods', "*")
     return response
 
-
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
